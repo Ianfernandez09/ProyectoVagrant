655202512919c9ec39030695baebb5ea5ea15a3b
Ianfernandez09 <ian.fernandez3@gmail.com>
2020-12-03 10:18:44 +0100

    actualizacion

--- instala_todo.sh
+++ instala_todo.sh
@@ -2,9 +2,9 @@
 apt-get install -y default-jdk
 
 # Instala tomcat9
-apt-get install tomcat9-docs tomcat9-examples tomcat9-admin
+apt-get install -y tomcat9-docs tomcat9-examples tomcat9-admin
 
 # Instala mariadb
-apt-get install mariadb-server
+apt-get install -y mariadb-server
 
 
